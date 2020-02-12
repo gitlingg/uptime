c78c09f66e62c72cd672d1c4d07027e02e23a95f
gitlingg <60963275+gitlingg@users.noreply.github.com>
2020-02-12 17:04:10 +0100

    Update uptimecheck.py

--- uptimecheck.py
+++ uptimecheck.py
@@ -42,6 +42,7 @@ def speedtest():
   global ping
   try:
     response = subprocess.Popen('/usr/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+    print(response)
     pingtime = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
     download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
     upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
